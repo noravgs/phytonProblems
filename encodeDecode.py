dd01d1b65e4097601463aa0e8ae6b4bcee4067de
Nora Vargas <noravgs92@gmail.com>
2024-08-06 14:28:30 -0500

    added refactored solution

--- encodeDecode.py
+++ encodeDecode.py
@@ -37,3 +37,22 @@ class Solution:
             # Move to the next encoded string
             i = j + 1 + length
         return decoded_strings
+
+#refactor code 
+from typing import List
+
+def encode(strs: List[str]) -> str:
+    encoded_string = ""
+    for s in strs:
+        encoded_string += str(len(s)) + "#" + s
+    return encoded_string
+
+def decode(s: str) -> List[str]:
+    i = 0
+    decoded_strings = []
+    while i < len(s):
+        j = s.find("#", i)
+        length = int(s[i:j])
+        decoded_strings.append(s[j + 1 : j + 1 + length])
+        i = j + 1 + length
+    return decoded_strings
